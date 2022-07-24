670a52e11131e3023d8c59d6d680362f041d5167
Muhammad Alhajee <alhajee2009@gmail.com>
2022-07-24 09:16:15 +0100

    refactor: style map ui

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,6 +5,7 @@ import {
   InfoWindow,
 } from "@react-google-maps/api";
 import { formatRelative } from "date-fns";
+import mapStyles from "../MapStyles";
 
 type LibraryTypes =
   | "places"
@@ -24,6 +25,12 @@ const center = {
   lng: 7.472469,
 };
 
+const options: google.maps.MapOptions = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+
 const Map = () => {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || "API_KEY",
@@ -35,10 +42,17 @@ const Map = () => {
 
   return (
     <div>
+      <h1 className="absolute m-4 z-10 text-3xl">
+        Hotspots
+        <span role="img" aria-label="fire" className="ml-2">
+          🔥
+        </span>
+      </h1>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         center={center}
         zoom={12}
+        options={options}
       ></GoogleMap>
     </div>
   );
